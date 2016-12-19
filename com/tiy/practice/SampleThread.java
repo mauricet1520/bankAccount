5b34a7fddf5675f21abc4d5138b6a27923dc109d
Maurice Thomas <mauricet1520@gmail.com>
2016-12-19 14:00:30 -0500

    another commit

--- com/tiy/practice/SampleThread.java
+++ com/tiy/practice/SampleThread.java
@@ -9,14 +9,16 @@ public class SampleThread implements Runnable {
     public void run() {
         System.out.println("Running " + Thread.currentThread().getId());
 
+        for (int i = 0; i < Thread.currentThread().getId() ; i++) {
+            
 
-        double newAmount =day7Runner.bank.listAccounts.get(0).getCurrentAmount() + 1.00;
+            double newAmount = day7Runner.bank.listAccounts.get(0).getCurrentAmount() + 1.00;
 
-        day7Runner.bank.listAccounts.get(0).setCurrentAmount(newAmount);
+            day7Runner.bank.listAccounts.get(0).setCurrentAmount(newAmount);
 
-        System.out.println("Current amount is " + newAmount);
+            System.out.println("Current amount is " + newAmount);
 
-
+        }
         try {
             Thread.sleep(8000);
         } catch (Exception exception) {
